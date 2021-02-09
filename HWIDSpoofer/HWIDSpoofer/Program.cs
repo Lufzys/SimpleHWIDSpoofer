b824dd31e32ac7a6ed570d09499f4460a0ea8194
Furkan "Lufzys <53129220+Lufzys@users.noreply.github.com>
2021-02-09 23:57:54 +0300

    added src

---
+++ HWIDSpoofer/HWIDSpoofer/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace HWIDSpoofer
+{
+    class Program
+    {
+        public static string textLogo = @"   ____  ____  ____      ____  _____  ______      ______   _______     ___      ___   ________  ________  _______    " + Environment.NewLine +
+            @"  |_   ||   _||_  _|    |_  _||_   _||_   _ `.  .' ____ \ |_   __ \  .'   `.  .'   `.|_   __  ||_   __  ||_   __ \   " + Environment.NewLine +
+            @"    | |__| |    \ \  /\  / /    | |    | | `. \ | (___ \_|  | |__) |/  .-.  \/  .-.  \ | |_ \_|  | |_ \_|  | |__) |  " + Environment.NewLine +
+            @"    |  __  |     \ \/  \/ /     | |    | |  | |  _.____`.   |  ___/ | |   | || |   | | |  _|     |  _| _   |  __ /   " + Environment.NewLine +
+            @"   _| |  | |_     \  /\  /     _| |_  _| |_.' / | \____) | _| |_    \  `-'  /\  `-'  /_| |_     _| |__/ | _| |  \ \_ " + Environment.NewLine +
+            @"  |____||____|     \/  \/     |_____||______.'   \______.'|_____|    `.___.'  `.___.'|_____|   |________||____| |___|" + Environment.NewLine; // for create ascii text signature https://www.kammerl.de/ascii/AsciiSignature.php
+
+        static void Main(string[] args) // https://github.com/Lufzys/SimpleHWIDSpoofer
+        {
+        begin:
+            Console.Title = "[LF1337] Simple Hwid Spoofer - github.com/Lufzys/SimpleHWIDSpoofer";
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine(textLogo);
+            Console.WriteLine("  ┌ FUNCS ──────────────────────────────────┐");
+            Console.WriteLine("  │ [!hwid] Spoof HWID                      │");
+            Console.WriteLine("  │ [!guid] Spoof Guid                      │");
+            Console.WriteLine("  │ [!pcName] Spoof your Computer Name      │");
+            Console.WriteLine("  │ [!productId] Spoof ProductId            │");
+            Console.WriteLine("  └─────────────────────────────────────────┘");
+
+            string input = Console.ReadLine();
+            if(input == "!hwid")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.HWID.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.HWID.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.HWID.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "!guid")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.PCGuid.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.PCGuid.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.PCGuid.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "!pcName")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.PCName.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.PCName.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.PCName.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "!productId")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.ProductId.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.ProductId.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.ProductId.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else
+            {
+                goto begin;
+            }
+        }
+    }
+}
---
+++ HWIDSpoofer/HWIDSpoofer/Spoofer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Text;
+using Microsoft.Win32;
+
+namespace HWIDSpoofer
+{
+    class Spoofer
+    {
+        public static class HWID
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
+            public static readonly string Key = "HwProfileGuid";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result =  SetValue("{" + Guid.NewGuid().ToString() + "}");
+                if (result)
+                {
+                    Log.Append("  [SPOOFER] HWID Changed from " + oldValue + " to " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [SPOOFER] Error accessing the Registry... Maybe run as admin");
+                }
+                return result;
+            }
+        }
+
+        public static class PCGuid
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SOFTWARE\Microsoft\Cryptography");
+            public static readonly string Key = "MachineGuid";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue(Guid.NewGuid().ToString());
+                if (result)
+                {
+                    Log.Append("  [SPOOFER] Guid Changed from " + oldValue + " to " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [SPOOFER] Error accessing the Registry... Maybe run as admin");
+                }
+                return result;
+            }
+        }
+
+        public static class PCName
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName");
+            public static readonly string Key = "ComputerName";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue("DESKTOP-" + Utilities.GenerateString(15));
+                if (result)
+                {
+                    Log.Append("  [SPOOFER] Computer Name Changed from " + oldValue + " to " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [SPOOFER] Error accessing the Registry... Maybe run as admin");
+                }
+                return result;
+            }
+        }
+
+        public static class ProductId
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+            public static readonly string Key = "ProductID";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue(Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5));
+                if(result)
+                {
+                    Log.AppendLine("  [SPOOFER] Computer ProductID Changed from " + oldValue + " to " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [SPOOFER] Error accessing the Registry... Maybe run as admin");
+                }
+                return result;
+            }
+        }
+
+        public class Regedit
+        {
+            private string regeditPath = string.Empty;
+            public Regedit(string regeditPath)
+            {
+                this.regeditPath = regeditPath;
+            }
+
+            public string Read(string keyName)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            return key.GetValue(keyName).ToString();
+                        }
+                        else
+                        {
+                            //Console.WriteLine("  [Regedit] SubKey Doesn't founded!");
+                            return "ERR";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("  [Regedit] Error accessing the Registry... Maybe run as admin?\n\n" + ex.ToString());
+                    return "ERR";
+                }
+            }
+
+            public bool Write(string keyName, object value)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            key.SetValue(keyName, value);
+                            return true;
+                        }
+                        else
+                        {
+                            //Console.WriteLine("  [Regedit] SubKey Doesn't founded!");
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("  [Regedit] Error accessing the Registry... Maybe run as admin? -" + ex.ToString());
+                    return false;
+                }
+            }
+        }
+
+        public static class Utilities
+        {
+            private static Random rand = new Random();
+            public const string Alphabet = "ABCDEF0123456789";
+            private static Random random = new Random();
+            public const string Alphabet1 = "abcdef0123456789";
+
+            public static string GenerateString(int size)
+            {
+                char[] array = new char[size];
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = Alphabet[rand.Next(Alphabet.Length)];
+                }
+                return new string(array);
+            }
+        }
+    }
+}
